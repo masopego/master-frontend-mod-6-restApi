2691db74a73bfc0bdd342a52ee299479e690fb3b
Marisol Pena <masopego@gmail.com>
2022-03-19 12:02:24 +0100

    Edit RMUrl from character collection

--- src/pods/character-collection/api/character-collection.api.ts
+++ src/pods/character-collection/api/character-collection.api.ts
@@ -1,7 +1,7 @@
 import Axios from 'axios';
 import { CharacterEntityApi } from './character-collection.api-model';
 
-const RMUrl = 'http://localhost:3000/characters/';
+const RMUrl = 'api/characters/';
 
 export const getCharacterCollection = async (): Promise<CharacterEntityApi> => {
   const { data } = await Axios.get(RMUrl);
